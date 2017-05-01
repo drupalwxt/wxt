10765a7f7fb1a41f31e800de07361ec150036eb3
William Hearn <sylus1984@gmail.com>
2017-04-30 22:24:44 -0400

    feat(testing): Adjusts tests to align with lightning structure

---
+++ tests/src/Unit/FormHelperTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\Tests\wxt\Unit;
+
+use Drupal\Core\Render\ElementInfoManagerInterface;
+use Drupal\wxt\FormHelper;
+use Drupal\Tests\UnitTestCase;
+
+/**
+ * @group wxt
+ *
+ * @coversDefaultClass \Drupal\wxt\FormHelper
+ */
+class FormHelperTest extends UnitTestCase {
+
+  /**
+   * @covers ::applyStandardProcessing
+   */
+  public function testApplyStandardProcessing() {
+    $element_info = $this->prophesize(ElementInfoManagerInterface::class);
+    $element_info->getInfo('location')->willReturn([
+      '#process' => [
+        'process_location',
+      ],
+    ]);
+    $element = ['#type' => 'location'];
+
+    $form_helper = new FormHelper($element_info->reveal());
+    $form_helper->applyStandardProcessing($element);
+
+    $this->assertEquals(['process_location'], $element['#process']);
+  }
+
+}
