2dea18519c5ebc4be98766ebda9ad92aa9522890
William H <sylus1984@gmail.com>
2017-07-16 20:01:07 -0400

    feat(d.o): Support legacy make file packaging (#27)

--- src/Composer/Package.php
+++ src/Composer/Package.php
@@ -9,7 +9,7 @@ use Composer\Util\ProcessExecutor;
 use Symfony\Component\Yaml\Yaml;
 
 /**
- * Generates Drush make files for drupal.org's packaging system.
+ * Generates Drush make files for drupal.org's ancient packaging system.
  */
 class Package {
 
@@ -72,9 +72,9 @@ class Package {
     foreach ($make['projects'] as $key => &$project) {
       if ($project['download']['type'] == 'git') {
         $tag = !empty($project['download']['tag']) ? $project['download']['tag'] : '';
-        preg_match('/[0-9]\.x-[0-9]*[0-9]*\.0/', $tag, $match);
+        preg_match('/\d+\.x-\d+\.0/', $tag, $match);
         $tag = str_replace($match, str_replace('x-', NULL, $match), $tag);
-        preg_match('/[0-9]\.[0-9]*[0-9]*\.0/', $tag, $match);
+        preg_match('/\d+\.\d+\.0/', $tag, $match);
         if (!empty($match[0])) {
           $tag = str_replace($match, substr($match[0], 0, -2), $tag);
           $project['version'] = $tag;
