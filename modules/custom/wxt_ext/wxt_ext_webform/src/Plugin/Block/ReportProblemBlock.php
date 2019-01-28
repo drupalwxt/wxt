d31bd1f2d7646d23bf63490ef449d81063991bd2
William Hearn <sylus1984@gmail.com>
2019-01-27 20:52:16 -0500

    feat(webform): Add report a problem block

---
+++ modules/custom/wxt_ext/wxt_ext_webform/src/Plugin/Block/ReportProblemBlock.php
@@ -0,0 +1,174 @@
+<?php
+
+namespace Drupal\wxt_ext_webform\Plugin\Block;
+
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\webform\WebformInterface;
+use Drupal\webform\WebformTokenManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+/**
+ * Provides a 'ReportProblemBlock' block.
+ *
+ * @Block(
+ *  id = "report_problem_block",
+ *  admin_label = @Translation("Report Problem block"),
+ *   category = @Translation("WxT Webform")
+ * )
+ */
+class ReportProblemBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The request stack.
+   *
+   * @var \Symfony\Component\HttpFoundation\RequestStack
+   */
+  protected $requestStack;
+
+  /**
+   * Entity type manager.
+   *
+   * @var \Drupal\core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * The webform token manager.
+   *
+   * @var \Drupal\webform\WebformTokenManagerInterface
+   */
+  protected $tokenManager;
+
+  /**
+   * Creates a ReportProblemBlock instance.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Symfony\Component\HttpFoundation\RequestStack $request_stack
+   *   The request stack.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   * @param \Drupal\webform\WebformTokenManagerInterface $token_manager
+   *   The webform token manager.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, RequestStack $request_stack, EntityTypeManagerInterface $entity_type_manager, WebformTokenManagerInterface $token_manager) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->requestStack = $request_stack;
+    $this->entityTypeManager = $entity_type_manager;
+    $this->tokenManager = $token_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('request_stack'),
+      $container->get('entity_type.manager'),
+      $container->get('webform.token_manager')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return [
+      'webform_id' => '',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+
+    $form['webform_id'] = [
+      '#title' => $this->t('Webform'),
+      '#type' => 'entity_autocomplete',
+      '#target_type' => 'webform',
+      '#required' => TRUE,
+      '#default_value' => $this->getWebform(),
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    $this->configuration['webform_id'] = $form_state->getValue('webform_id');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+
+    $url = $this->getWebform()->toUrl()
+      ->setOption('query', $this->requestStack->getCurrentRequest()->query->all())
+      ->toString();
+    $markup = '<a class="btn btn-default" href="' . $url . '">';
+    $markup .= '<span class="glyphicon glyphicon-play">&nbsp;</span>';
+    $markup .= $this->t('Report a problem on this page');
+    $markup .= '</a>';
+    $build['#markup'] = $markup;
+    return $build;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function blockAccess(AccountInterface $account) {
+    $webform = $this->getWebform();
+    if (!$webform) {
+      return AccessResult::forbidden();
+    }
+
+    $access_result = $webform->access('submission_create', $account, TRUE);
+    if ($access_result->isAllowed()) {
+      return $access_result;
+    }
+
+    $has_access_denied_message = ($webform->getSetting('form_access_denied') !== WebformInterface::ACCESS_DENIED_DEFAULT);
+    return AccessResult::allowedIf($has_access_denied_message)
+      ->addCacheableDependency($access_result);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function calculateDependencies() {
+    $dependencies = parent::calculateDependencies();
+
+    $webform = $this->getWebform();
+    $dependencies[$webform->getConfigDependencyKey()][] = $webform->getConfigDependencyName();
+
+    return $dependencies;
+  }
+
+  /**
+   * Get this block instance webform.
+   *
+   * @return \Drupal\webform\WebformInterface
+   *   A webform or NULL.
+   */
+  protected function getWebform() {
+    return $this->entityTypeManager->getStorage('webform')->load($this->configuration['webform_id']);
+  }
+
+}
