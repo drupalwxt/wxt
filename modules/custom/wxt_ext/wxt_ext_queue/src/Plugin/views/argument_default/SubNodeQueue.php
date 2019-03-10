fa398235e63ce6baf696889cfff9536237510693
William Hearn <sylus1984@gmail.com>
2019-03-10 14:43:57 -0400

    feat(entityqueue): Add new contrib

---
+++ modules/custom/wxt_ext/wxt_ext_queue/src/Plugin/views/argument_default/SubNodeQueue.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Drupal\wxt_ext_queue\Plugin\views\argument_default;
+
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\node\NodeStorageInterface;
+use Drupal\node\Plugin\views\argument_default\Node;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Default argument plugin to get the current node's queue.
+ *
+ * @ViewsArgumentDefault(
+ *   id = "subnodequeue",
+ *   title = @Translation("SubNodeQueue context from current node")
+ * )
+ */
+class SubNodeQueue extends Node {
+
+  /**
+   * The node storage controller.
+   *
+   * @var \Drupal\node\NodeStorageInterface
+   */
+  protected $nodeStorage;
+
+  /**
+   * Constructs a Drupal\wxt_ext_queue\Plugin\views\argument_default\SubNodeQueue.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param array $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
+   *   The route match.
+   * @param \Drupal\node\NodeStorageInterface $node_storage
+   *   The node storage controller.
+   */
+  public function __construct(array $configuration, $plugin_id, array $plugin_definition, RouteMatchInterface $route_match, NodeStorageInterface $node_storage) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $route_match);
+    $this->nodeStorage = $node_storage;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('current_route_match'),
+      $container->get('entity.manager')->getStorage('node')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getArgument() {
+    // Use the argument_default_node plugin to get the nid argument.
+    $nid = parent::getArgument();
+    if (!empty($nid)) {
+      $node = $this->nodeStorage->load($nid);
+      if ($node->hasField('field_queue')) {
+        return $node->get('field_queue')->getString();
+      }
+    }
+  }
+
+}
---
+++ modules/custom/wxt_ext/wxt_ext_queue/src/Plugin/views/argument_default/SubTaxonomyQueue.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Drupal\wxt_ext_queue\Plugin\views\argument_default;
+
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\taxonomy\TermInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\taxonomy\VocabularyStorageInterface;
+use Drupal\taxonomy\Plugin\views\argument_default\Tid;
+
+/**
+ * Taxonomy tid default argument.
+ *
+ * @ViewsArgumentDefault(
+ *   id = "subtaxonomyqueue",
+ *   title = @Translation("Taxonomy term ID from Context of the page")
+ * )
+ */
+class SubTaxonomyQueue extends Tid {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $routeMatch;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $vocabularyStorage;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, RouteMatchInterface $route_match, VocabularyStorageInterface $vocabulary_storage) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $route_match, $vocabulary_storage);
+
+    $this->routeMatch = $route_match;
+    $this->vocabularyStorage = $vocabulary_storage;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('current_route_match'),
+      $container->get('entity.manager')->getStorage('taxonomy_vocabulary')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getArgument() {
+    // Load default argument from taxonomy page.
+    if (!empty($this->options['term_page'])) {
+      if (($taxonomy_term = $this->routeMatch->getParameter('taxonomy_term')) && $taxonomy_term instanceof TermInterface) {
+        return $taxonomy_term->id();
+      }
+    }
+  }
+
+}
