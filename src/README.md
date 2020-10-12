95089814345995fe196cbc8e3b12378311c634c8
William Hearn <sylus1984@gmail.com>
2020-10-12 12:49:02 -0400

    feat(config): Add Environment Detector

---
+++ src/README.md
@@ -0,0 +1,3 @@
+## Acknowledgements
+
+Most of these PSR classes come from the amazing work done by [Acquia Team](https://www.acquia.com/) over at [Lightning](https://github.com/acquia/lightning) and [BLT](https://github.com/acquia/blt).
---
+++ src/Robo/Common/EnvironmentDetector.php
@@ -0,0 +1,173 @@
+<?php
+
+namespace Drupal\wxt\Robo\Common;
+
+/**
+ * Class EnvironmentDetector.
+ *
+ * @package Drupal\wxt\Robo\Common
+ *
+ * Attempts to detect various properties about the current hosting environment.
+ */
+class EnvironmentDetector {
+
+  /**
+   * Get CI env name.
+   *
+   * In the case of multiple environment detectors declaring a CI env name, the
+   * first one wins.
+   */
+  public static function getCiEnv() {
+    $results = self::getSubclassResults(__FUNCTION__);
+    if ($results) {
+      return current($results);
+    }
+
+    $mapping = [
+      'GITHUB' => 'github',
+      'PIPELINE_ENV' => 'pipelines',
+    ];
+    foreach ($mapping as $env_var => $ci_name) {
+      if (getenv($env_var)) {
+        return $ci_name;
+      }
+    }
+    return FALSE;
+  }
+
+  /**
+   * Is CI.
+   */
+  public static function isCiEnv() {
+    return self::getCiEnv() || getenv('CI');
+  }
+
+  /**
+   * Get the settings file include for the current CI environment.
+   *
+   * This may be provided by WxT, or via a Composer package that has provided
+   * its own environment detector. In the case of multiple detectors providing a
+   * settings file, the first one wins.
+   *
+   * @return string
+   *   Settings file full path and filename.
+   */
+  public static function getCiSettingsFile() {
+    $results = array_filter(self::getSubclassResults(__FUNCTION__));
+    if ($results) {
+      return current($results);
+    }
+
+    return sprintf("%s/vendor/drupalwxt/wxt/settings/%s.settings.php", dirname(DRUPAL_ROOT), self::getCiEnv());
+  }
+
+  /**
+   * Is local.
+   */
+  public static function isLocalEnv() {
+    return !self::isCiEnv();
+  }
+
+  /**
+   * Is dev.
+   */
+  public static function isDevEnv() {
+    $results = self::getSubclassResults(__FUNCTION__);
+    if ($results) {
+      return TRUE;
+    }
+  }
+
+  /**
+   * Is stage.
+   */
+  public static function isStageEnv() {
+    $results = self::getSubclassResults(__FUNCTION__);
+    if ($results) {
+      return TRUE;
+    }
+  }
+
+  /**
+   * Is prod.
+   */
+  public static function isProdEnv() {
+    $results = self::getSubclassResults(__FUNCTION__);
+    if ($results) {
+      return TRUE;
+    }
+  }
+
+  /**
+   * Find the repo root.
+   *
+   * This isn't as trivial as it sounds, since a simple relative path
+   * (__DIR__ . '/../../../../../../') won't work if this package is symlinked
+   * using a Composer path repository, and this file can be invoked from both
+   * web requests and WxT CLI calls.
+   *
+   * @return string
+   *   The repo root as an absolute path.
+   */
+  public static function getRepoRoot() {
+    if (defined('DRUPAL_ROOT')) {
+      // This is a web or Drush request.
+      return dirname(DRUPAL_ROOT);
+    }
+  }
+
+  /**
+   * List detectable environments and whether they are currently active.
+   */
+  public static function getEnvironments() {
+    return [
+      'local' => self::isLocalEnv(),
+      'dev' => self::isDevEnv(),
+      'stage' => self::isStageEnv(),
+      'prod' => self::isProdEnv(),
+      'ci' => self::isCiEnv(),
+    ];
+  }
+
+  /**
+   * Call a given function in all EnvironmentDetector subclasses.
+   *
+   * Composer packages can provide their own version of an EnvironmentDetector
+   * that inherits from this one. This allows for detection of new types of
+   * environments not hardcoded in this class.
+   *
+   * @param string $functionName
+   *   The function name to call in a subclass.
+   *
+   * @return array
+   *   Results from each subclass function call (omits any null / false results)
+   *
+   * @throws \ReflectionException
+   */
+  private static function getSubclassResults($functionName) {
+    static $detectors;
+    if (!isset($detectors)) {
+      $autoload_file = self::getRepoRoot() . '/vendor/autoload.php';
+      if (!file_exists($autoload_file)) {
+        $detectors = [];
+        return [];
+      }
+      // phpcs:ignore
+      $autoloader = require $autoload_file;
+      $classMap = $autoloader->getClassMap();
+      $detectors = array_filter($classMap, function ($classPath) {
+        return strpos($classPath, 'WxT/Plugin/EnvironmentDetector') !== FALSE;
+      });
+    }
+    $results = [];
+    foreach ($detectors as $detector => $classPath) {
+      // Only call this method if it's been overridden by the child class.
+      $detectorReflector = new \ReflectionMethod($detector, $functionName);
+      if ($detectorReflector->getDeclaringClass()->getName() === $detector) {
+        $results[] = call_user_func([$detector, $functionName]);
+      }
+    }
+    return array_filter($results);
+  }
+
+}
