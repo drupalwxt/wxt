f96dded4ae4377dfa1a25a3929cb8d8455c6f78c
William Hearn <sylus1984@gmail.com>
2021-01-26 12:54:36 -0500

    composer require --no-update "ext-dom:*" cweagans/composer-patches oomphinc/composer-installers-extender 'drupal/core:9.0.11'

--- tarball.sh
+++ tarball.sh
@@ -12,7 +12,7 @@ if [[ ! $YAML_CLI ]]; then
 fi
 
 rm -rf $ARCHIVE
-composer create-project --stability beta --no-install drupal/legacy-project:9.0.* $ARCHIVE
+composer create-project --stability beta --no-install drupal/legacy-project:9.0.11 $ARCHIVE
 composer dump-autoload
 composer configure-tarball $ARCHIVE
 
@@ -30,7 +30,7 @@ composer config extra.enable-patching true
 composer config minimum-stability dev
 composer config prefer-stable true
 composer remove --no-update composer/installers
-composer require --no-update "ext-dom:*" cweagans/composer-patches oomphinc/composer-installers-extender
+composer require --no-update "ext-dom:*" cweagans/composer-patches oomphinc/composer-installers-extender 'drupal/core:9.0.11'
 composer update
 
 # Create the profile destination directory.
