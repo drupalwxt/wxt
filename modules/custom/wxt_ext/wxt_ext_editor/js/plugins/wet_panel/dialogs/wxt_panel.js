f492d6560f4762468f08a8332b80ca76e510b701
William Hearn <sylus1984@gmail.com>
2021-07-19 13:24:34 -0400

    Squashed commit of the following:
    commit 18bd1c06108ac6072242058446e6855267f9e30c
    Author: smulvihill <smulvihill@795442.no-reply.drupal.org>
    Date:   Mon Jul 19 13:19:28 2021 -0400
    
        Issue #3224074 by smulvih2: Update password policy
    
    commit 3dc6a6701aca6c0364cc432284514ef11cca978a
    Author: smulvihill <smulvihill@795442.no-reply.drupal.org>
    Date:   Mon Jul 19 13:17:32 2021 -0400
    
        Issue #3224069 by smulvih2: CKEditor improvements - Panel dialog

---
+++ modules/custom/wxt_ext/wxt_ext_editor/js/plugins/wet_panel/dialogs/wxt_panel.js
@@ -0,0 +1,85 @@
+/**
+ * WET Panel - Dialog
+ */
+
+CKEDITOR.dialog.add('wet_panel', function(editor) {
+  return {
+    title: 'Panel configuration',
+    minWidth: 300,
+    minHeight: 120,
+    contents: [
+      {
+        id: 'info',
+        elements: [
+          {
+            id: 'type',
+            type: 'select',
+            label: 'Panel type',
+            items: [
+              ['primary', 'primary'],
+              ['success', 'success'],
+              ['info', 'info'],
+              ['warning', 'warning'],
+              ['danger', 'danger']
+            ],
+            'default': 'primary',
+            setup: function(element) {
+              // Setup the dialog field value on existing element
+              if (element.hasClass('panel-primary'))
+                this.setValue('primary');
+              else if (element.hasClass('panel-success'))
+                this.setValue('success');
+              else if (element.hasClass('panel-info'))
+                this.setValue('info');
+              else if (element.hasClass('panel-warning'))
+                this.setValue('warning');
+              else if (element.hasClass('panel-danger'))
+                this.setValue('danger');
+            },
+            commit: function(element) {
+              // Remove class from existing widget
+              element.removeClass('panel-primary');
+              element.removeClass('panel-success');
+              element.removeClass('panel-info');
+              element.removeClass('panel-warning');
+              element.removeClass('panel-danger');
+
+              // Add class
+              element.addClass('panel-' + this.getValue());
+            }
+          }
+        ]
+      }
+    ],
+    onShow: function(event) {
+      // Gets the DOM element in which the selection starts.
+      var element = editor.getSelection().getStartElement();
+
+      // Determine if we add a new element or modify existing element
+      if (element) {
+        element = element.getAscendant('section', true);
+      }
+
+      if (!element)
+        this.insertMode = true;
+      else
+        this.insertMode = false;
+
+      this.element = element;
+
+      // If editing an existing widget setup the dialog form
+      if (!this.insertMode) {
+        this.setupContent(this.element);
+      }
+    },
+    onOk: function(event) {
+      // On save of the dialog form, if editing an existing widget
+      if (!this.insertMode) {
+        // Change classes as needed
+        this.commitContent(this.element);
+        // Stop event to prevent new widget from being added
+        event.stop();
+      }
+    }
+  };
+});
