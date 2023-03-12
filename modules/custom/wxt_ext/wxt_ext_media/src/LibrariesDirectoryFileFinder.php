c45bbf94e86867c004ba6dbd16fed9e66465a47c
sylus <sylus1984@gmail.com>
2023-03-12 12:36:46 -0400

    feat(phpcs): Correct errors reported by phpcs

--- modules/custom/wxt_ext/wxt_ext_media/src/LibrariesDirectoryFileFinder.php
+++ modules/custom/wxt_ext/wxt_ext_media/src/LibrariesDirectoryFileFinder.php
@@ -30,4 +30,3 @@ final class LibrariesDirectoryFileFinder extends CoreLibrariesDirectoryFileFinde
   }
 
 }
-
--- modules/custom/wxt_ext/wxt_ext_media/src/WxTMediaServiceProvider.php
+++ modules/custom/wxt_ext/wxt_ext_media/src/WxTMediaServiceProvider.php
@@ -28,4 +28,3 @@ final class WxTMediaServiceProvider extends ServiceProviderBase {
   }
 
 }
-
