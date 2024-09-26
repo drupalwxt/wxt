acc863d19c33dc2e97b483b199458b14b0e30f57
Stephen Mulvihill <smulvih2@gmail.com>
2024-09-26 13:24:23 -0700

    Add ArchivedField.php constructor back to fix install error

--- modules/custom/wxt_ext/wxt_ext_archived/src/Plugin/Condition/ArchivedField.php
+++ modules/custom/wxt_ext/wxt_ext_archived/src/Plugin/Condition/ArchivedField.php
@@ -26,6 +26,23 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 class ArchivedField extends ConditionPluginBase implements ContainerFactoryPluginInterface {
 
   /**
+   * Creates a new ArchivedField instance.
+   *
+   * @param array $configuration
+   *   The plugin configuration, i.e. an array with configuration values keyed
+   *   by configuration option name. The special key 'context' may be used to
+   *   initialize the defined contexts by setting it to an array of context
+   *   values keyed by context names.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin_definition for the plugin instance.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+  }
+
+  /**
    * {@inheritdoc}
    */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
