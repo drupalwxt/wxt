5a5318fd653698e41c75fac13597faa86caec526
Stephen Mulvihill <smulvih2@gmail.com>
2025-12-14 10:38:24 -0800

    Issue #3562743 by joseph.olstad, smulvih2: Update migration labels and content - Drupal 10 instead of Drupal 9

--- modules/custom/wxt_ext/wxt_ext_migration/src/Plugin/migrate/process/UUIDLink.php
+++ modules/custom/wxt_ext/wxt_ext_migration/src/Plugin/migrate/process/UUIDLink.php
@@ -16,7 +16,7 @@ use Drupal\node\Entity\Node;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * Convert a Drupal 7 UUID Link to a Drupal 9 UUID Link.
+ * Convert a Drupal 7 UUID Link to a Drupal 10 UUID Link.
  *
  * @MigrateProcessPlugin(
  *   id = "wxt_uuid_link",
@@ -90,7 +90,7 @@ class UUIDLink extends ProcessPluginBase implements ContainerFactoryPluginInterf
   }
 
   /**
-   * Replace callback to convert a Drupal 7 UUID link into a Drupal 9 UUID Link.
+   * Replace callback to convert a Drupal 7 UUID link into a Drupal 10 UUID Link.
    *
    * @param string $match
    *   Takes a match of tag code.
